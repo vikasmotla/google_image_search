b9f2537618412e8dc98e94ef9e6abac2e2acdefb
vikas <vikas.motla@gmail.com>
2019-04-04 08:08:11 +0530

    db atlas added

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ const searchRoutes = require('./api/routes/search');
 const historyRoutes = require('./api/routes/history');
 
 
-var mongoDB = 'mongodb://localhost/zillion_db3';
-mongoose.connect(mongoDB);
+// var mongoDB = 'mongodb://localhost/zillion_db3';
+var mongoDB = 'mongodb+srv://vikas:zillion-search@cluster0-6cyxk.mongodb.net/test?retryWrites=true'
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
 
-mongoose.connection.once('open', function () {
+mongoose.connection.once('open', function() {
   console.log('Connection has been made');
-}).on('error',function (erro) {
+}).on('error', function(erro) {
   console.log('connection error');
   // console.log(error)
 })
 
-app.use('/static',express.static('static'));
+app.use('/static', express.static('static'));
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json());
 
@@ -37,7 +40,7 @@ app.use((res, req, next) => {
 // Routes which should habdle requests
 app.use('/search', searchRoutes);
 app.use('/history', historyRoutes);
-app.get('/',function (req,res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html')
 })
 
